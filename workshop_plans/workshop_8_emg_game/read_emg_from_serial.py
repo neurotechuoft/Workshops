3555ec64252dc712a746776152f4d74a5acbf144
a1bertle <albert.le6446@gmail.com>
2018-02-24 23:23:58 -0500

    Encode request into a byte array before sending over serial. Add timeout to wait for data. Add main()

--- workshop_plans/workshop_8_emg_game/read_emg_from_serial.py
+++ workshop_plans/workshop_8_emg_game/read_emg_from_serial.py
@@ -1,31 +1,45 @@
 import serial
 import time
 
-# Replace /dev/ttyACM0 with the serial port that your Arduino is connected to.
-# You should be able to view this at the bottom right corner of the Arduino IDE 
-# window as "Arduino Uno on <port name>"
-serialArduino = serial.Serial('/dev/ttyACM0', 115200)
+# Initialize the Serial object by calling: serial.Serial(<port_name>, <baud_rate>)
+# Replace 'COM3' below with the port that your Arduino is connected to, which
+# is indicated in the bottom right corner of the Arduino IDE window as "Arduino Nano on <port name>"
+serialArduino = serial.Serial('COM3', 9600)
 print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
 
 def get_emg_sample():
-    # Send a request to the Arduino for a sample
-    serialArduino.write('?')
-    
+
+    # Encode and send a request to the Arduino for a sample
+    serialArduino.write(('?').encode('utf-8'))
+
     # inWaiting() returns the number of bytes available to read from
-    # the serial port. If there is nothing for us to read, we keep
-    # waiting
-    while (serialArduino.inWaiting()==0):
-         pass
-    data = int(serialArduino.readline().strip());
+    # the serial buffer.
+    # We wait for 1000 counts then stop if nothing is available.
+    # This timeout prevents us from waiting forever if there is some problem
+    cnt = 0
+    timeout = 1000
+
+    while serialArduino.inWaiting()==0 and cnt <= timeout:
+        cnt += 1                                     # Increment the counter
+
+    if cnt >= timeout:
+        data = 0                                     # If we reached our timeout, assign data to 0, because there is nothing to read
+    else:
+        data = int(serialArduino.readline().strip()) # Otherwise, read whatever we saw in the serial buffer
 
     return data
 
-while True:
- 	sample = get_emg_sample()
- 	if (sample == 1):
- 		print "Received data: ", sample, "-----------------"
- 	else:
- 		print "Received data: ", sample
+def main():
 
- 	# Wait 33 ms to imitate a 30 fps drawing loop
- 	time.sleep(0.033)
+    while True:
+        sample = get_emg_sample()
+        if sample == 1:
+            print("Received data: ", sample, "-----------------")
+        else:
+            print("Received data: ", sample)
+
+        # Wait 33 ms to imitate a 30 fps drawing loop
+        time.sleep(0.033)
+
+if __name__ == "__main__":
+    main()
